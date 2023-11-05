22850286e57f0bcc114dc17b94610f27c23cd14d
ISSOtm <me@eldred.fr>
2023-11-05 17:23:30 +0100

    Remove tool obsoleted by RGBGFX 0.6.0

--- src/tools/bit7ify.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from sys import argv
-
-
-with open(argv[1], "rb") as input:
-    with open(argv[2], "wb") as output:    
-        output.write(bytes((byte ^ 0x80  for byte in input.read())))
